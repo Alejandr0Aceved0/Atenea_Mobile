ac37e5eafa6e50eff544a006fd6774d51de88ff4
Alejandro <alejossena12@gmail.com>
2023-07-09 18:23:49 -0500

    feature: getting views id components

--- app/src/main/java/com/ingeacev/reto1movil/MainActivity.java
+++ app/src/main/java/com/ingeacev/reto1movil/MainActivity.java
@@ -3,12 +3,25 @@ package com.ingeacev.reto1movil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText numberA, numberB;
+    Button btnSumar, btnRestar;
+    TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberA = findViewById(R.id.etNumA);
+        numberB = findViewById(R.id.etNumB);
+        btnSumar = findViewById(R.id.btnSumar);
+        btnRestar = findViewById(R.id.btnRestar);
+        tvResultado = findViewById(R.id.txtResult);
     }
 }
