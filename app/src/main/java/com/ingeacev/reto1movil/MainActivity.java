8cd763ee5a61bba1875a52ca91e96f30b4220d36
Alejandro <alejossena12@gmail.com>
2023-07-09 20:00:35 -0500

    feature: btnSumar and btnRestar function added

--- app/src/main/java/com/ingeacev/reto1movil/MainActivity.java
+++ app/src/main/java/com/ingeacev/reto1movil/MainActivity.java
@@ -3,13 +3,14 @@ package com.ingeacev.reto1movil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText numberA, numberB;
+    EditText etNumA, etNumB;
     Button btnSumar, btnRestar;
     TextView tvResultado;
 
@@ -18,10 +19,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numberA = findViewById(R.id.etNumA);
-        numberB = findViewById(R.id.etNumB);
+        etNumA = findViewById(R.id.etNumA);
+        etNumB = findViewById(R.id.etNumB);
         btnSumar = findViewById(R.id.btnSumar);
         btnRestar = findViewById(R.id.btnRestar);
         tvResultado = findViewById(R.id.txtResult);
+
+        //Cuando se de clic al boton sumar, ejecuta lo que esta dentro de public void onClick
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double numA = Double.parseDouble(etNumA.getText().toString());
+                double numB = Double.parseDouble(etNumB.getText().toString());
+                String rta = String.valueOf(numA + numB);
+                tvResultado.setText(getString(R.string.txt_resultado)+" "+rta);
+            }
+        });
+
+        btnRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double numA = Double.parseDouble(etNumA.getText().toString());
+                double numB = Double.parseDouble(etNumB.getText().toString());
+                String rta = String.valueOf(numA - numB);
+                tvResultado.setText(getString(R.string.txt_resultado)+" "+rta);
+            }
+        });
     }
 }
